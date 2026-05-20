75c97e3565d5e3d9032a05bff83a4dd50a23f147
KarnamShyam1947 <karnam.shyam2004@gmail.com>
2026-05-20 22:31:11 +0530

    Simple MCP server, with two tools

---
+++ server.py
@@ -0,0 +1,21 @@
+from mcp.server.fastmcp import FastMCP
+
+# Create server
+mcp = FastMCP("LearningMCP")
+
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+@mcp.tool()
+def greet(name: str) -> str:
+    """Greet a user"""
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
+    
